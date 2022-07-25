5282f6ee91efbe67be7e42bcdc3b7a838f96b631
Jason Holt <jason.grant.holt@gmail.com>
2022-07-25 14:29:04 -0400

    tested changing tenant name

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "aci" {
 
 module "my_tenant" {
   source = "github.com/jasongholt/aci_tenant_demo/modules/tenant"
-  tenant = "banking_app"
+  tenant = "banking_app2"
 }
 
 #The following module creates the VRF inside of the tenant.  You can customize the
